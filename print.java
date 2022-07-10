ee0a8ea96c299f3f037f9b0235d07eb6456f05cf
pradeep poddar <pradeeppoddar.iitkgp@gmail.com>
2022-07-10 11:15:36 +0900

    removed

--- print.java
+++
@@ -1,9 +0,0 @@
-public class print {
-
-    public static void main(String[] args) {
-
-
-        System.out.print("Enter a number: ");
-    
-}
-}
--- print.py
+++
@@ -1,6 +0,0 @@
-print("hello world")
-a=2
-b=3
-c=34
-print(a+b)
-# thank
